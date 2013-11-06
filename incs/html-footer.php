1b075a2e1ff5dc1cd0d94ce6c0f002215edabdec
JoeyD <williamdurham@gmail.com>
2013-11-06 15:10:03 -0500

    FitVid Support

--- incs/html-footer.php
+++ incs/html-footer.php
@@ -2,5 +2,10 @@
 	<?php wp_footer(); ?>
     </div>
     </div>
+    <script>
+        jQuery(document).ready(function(){
+            jQuery(".container").fitVids();
+        });
+    </script>
 	</body>
 </html>
