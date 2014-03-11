dfb5202ea7a371cbb8b27455867dc1ce25faa929
JoeyD <williamdurham@gmail.com>
2014-03-11 17:25:39 -0400

    Remove test code

--- page.php
+++ page.php
@@ -2,9 +2,7 @@
 
   <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
   <h1><?php the_title(); ?></h1>
-  <div class="gray">
 
-  </div>
   <?php the_content(); ?>
   <?php //comments_template( '', true ); ?>
   <?php endwhile; ?>
